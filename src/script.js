d01805a3aad0b3df4b97ab89a87e296baeb6c568
Francisco Luis Campo <fcampo7@hotmail.com>
2024-08-28 12:05:35 -0300

    Fix: Vista mobile

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// Get all "navbar-burger" elements
 	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
+
 	// Add a click event on each of them
 	$navbarBurgers.forEach( el => {
 	  el.addEventListener('click', () => {
@@ -14,11 +14,10 @@ document.addEventListener('DOMContentLoaded', () => {
 		// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
 		el.classList.toggle('is-active');
 		$target.classList.toggle('is-active');
-  
-	  });
+		
+		});
 	});
-  
-  });
+});
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
