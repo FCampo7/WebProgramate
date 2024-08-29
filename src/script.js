1c262233cdf95bdd674bbf6c7bab6b0933435832
Francisco Luis Campo <fcampo7@hotmail.com>
2024-08-29 17:17:13 -0300

    Add: modals Change: columns a grids

--- src/script.js
+++ src/script.js
@@ -1,8 +1,9 @@
+// Lógica de la barra de navegación
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Get all "navbar-burger" elements
 	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-	const navbarItems = document.querySelectorAll('.navbar-item');
+	const navbarItems = document.querySelectorAll('.ni');
 	// Add a click event on each of them
 	$navbarBurgers.forEach( el => {
 	  el.addEventListener('click', () => {
@@ -26,6 +27,9 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 });
 
+
+
+// Lógica del botón de go to top
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
@@ -42,3 +46,49 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+
+// Lógica de los modals
+document.addEventListener('DOMContentLoaded', () => {
+	// Functions to open and close a modal
+	function openModal($el) {
+	  $el.classList.add('is-active');
+	}
+  
+	function closeModal($el) {
+	  $el.classList.remove('is-active');
+	}
+  
+	function closeAllModals() {
+	  (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+		closeModal($modal);
+	  });
+	}
+  
+	// Add a click event on buttons to open a specific modal
+	(document.querySelectorAll('#myModal') || []).forEach(($trigger) => {
+	  const modal = $trigger.dataset.target;
+	  const $target = document.getElementById(modal);
+  
+	  $trigger.addEventListener('click', () => {
+		openModal($target);
+	  });
+	});
+  
+	// Add a click event on various child elements to close the parent modal
+	(document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+	  const $target = $close.closest('.modal');
+  
+	  $close.addEventListener('click', () => {
+		closeModal($target);
+	  });
+	});
+  
+	// Add a keyboard event to close all modals
+	document.addEventListener('keydown', (event) => {
+	  if(event.key === "Escape") {
+		closeAllModals();
+	  }
+	});
+  });
